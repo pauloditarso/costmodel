44f3768d7f14f0b5376253a4bd2ef44362f54411
Paulo Ditarso <paulo.maciel@ifpb.edu.br>
2019-08-21 12:25:11 +0300

    update

--- decomposeProv.R
+++ decomposeProv.R
@@ -1,19 +1,26 @@
 decomposeProv <- function(providers, resource) {
 
-	
-
 	auxProv <- data.frame()
 
-	for (i in 1:numberOfProviders) {
+	for (i in 1:length(providers)) {
 
-		for ( j in 1:length(providers[[i]][[paste(resource)]]) ) {
+		if ( length(providers[[i]][[paste(resource)]][[1]]) == 3 ) {
 
-			# auxProv <- rbind(auxProv, providers[[i]][[paste(resource)]][j])
-			print(paste("i", i, "j", j))
+			for ( j in 1:length(providers[[i]][[paste(resource)]]) ) {
+
+				arg1 <- as.numeric(providers[[i]][[paste(resource)]][[j]][1])
+				arg2 <- as.numeric(providers[[i]][[paste(resource)]][[j]][2])
+				arg3 <- as.numeric(providers[[i]][[paste(resource)]][[j]][3])
+
+				# auxProv <- rbind( auxProv, data.frame(providerID=i, resourceType=paste(resource), resourceID=j, arg1=arg1, arg2=arg2, arg3=arg3) )
+				auxProv <- rbind( auxProv, data.frame(providerID=i, resourceID=j, arg1=arg1, arg2=arg2, arg3=arg3) )
+				
+			}
+
 		}
 
 	} 
 
-	# auxProv
+	auxProv
 
 }
--- decomposeSP.R
+++ decomposeSP.R
@@ -1,8 +1,14 @@
 decomposeSP <- function(SP, resource) {
 
 	SPaux <- data.frame()
+	
+	if ( length(SP[[paste(resource)]][[1]]) == 3 ) {
 
-	for (i in 1:length(SP[[paste(resource)]])) { SPaux <- rbind(SPaux, SP$hosts[[i]]) }
+		for (i in 1:length(SP[[paste(resource)]])) { 
+			SPaux <- rbind(SPaux, SP[[paste(resource)]][[i]]) 
+		}
+
+	}
 
 	SPaux
 	
