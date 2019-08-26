7c60afa34a3aefd5eb11627d5f2f0553e5eaf5bc
Paulo Ditarso <paulo.maciel@ifpb.edu.br>
2019-08-26 12:24:08 +0300

    indexes.R

---
+++ indexes.R
@@ -0,0 +1,25 @@
+indexes <- function(numberOfRequests, numberOfResources) {
+
+	library(gtools)
+
+	requests <- permutations(numberOfRequests, numberOfRequests)
+	resources <- combinations(numberOfResources, numberOfRequests)
+
+	if ( ncol(requests) > nrow(resources) ) return -1
+
+	allCombinations <- data.frame()
+
+	for ( i in 1:nrow(requests) ) {
+
+		for ( j in 1:nrow(resources) ) {
+
+			aux <- c(requests[i,], resources[j,])
+			allCombinations <- rbind(allCombinations, aux)
+
+		}
+
+	}
+
+	allCombinations
+
+}
