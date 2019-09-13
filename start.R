6ef491d9e27b75ff8fae57cf7fc2fd08496960cf
Paulo Ditarso <paulo.maciel@ifpb.edu.br>
2019-09-13 17:28:31 +0300

    updating start.R

--- start.R
+++ start.R
@@ -1,12 +1,12 @@
 rm(list = ls())
 source('./sourceAll.R')
-SPConfig<-c(3,2,1)
-numberOfProviders<-3
+SPConfig<-c(2,2,1)
+numberOfProviders<-1
 priceHostPerDay<-100
 priceLinkPerDay<-100
 priceNEPerDay<-100
 
-print("defining reference resources!!")
+#print("defining reference resources!!")
 referenceHost <- c(1, 4, 128, 0.1)
 referenceLink <- c(1, 1, 4, 0.2)
 referenceNE <- c(1, 6, 1, 0.2)
@@ -21,7 +21,7 @@ if ( !exists("priceNEPerDay") ) {
   priceNEPerDay <- 25
 }
 
-print("defining SP resources!!")
+#print("defining SP resources!!")
 if ( exists("SPConfig") ) {
   SP <- createOneSP(SPConfig[1], SPConfig[2], SPConfig[3])
 } else {
@@ -32,7 +32,7 @@ SPhosts <- decomposeSP(SP, "hosts")
 SPlinks <- decomposeSP(SP, "links")
 SPnes <- decomposeSP(SP, "nes")
 
-print("defining providers resources!!")
+#print("defining providers resources!!")
 if ( exists("numberOfProviders") ) {
   P <- createProviders(numberOfProviders, 4, 4, 2)
 } else {
@@ -40,24 +40,24 @@ if ( exists("numberOfProviders") ) {
 }
 
 Phosts <- decomposeProv(P, "hosts")
-print(paste(nrow(Phosts), "hosts"))
+#print(paste(nrow(Phosts), "hosts"))
 Plinks <- decomposeProv(P, "links")
-print(paste(nrow(Plinks), "links"))
+#print(paste(nrow(Plinks), "links"))
 Pnes <- decomposeProv(P, "nes")
-print(paste(nrow(Pnes), "nes"))
+#print(paste(nrow(Pnes), "nes"))
 
-print("defining hosts combinations!!")
+#print("defining hosts combinations!!")
 hostsComb <- indexes(nrow(SPhosts), nrow(Phosts))
-print("defining links combinations!!")
+#print("defining links combinations!!")
 linksComb <- indexes(nrow(SPlinks), nrow(Plinks))
-print("defining nes combinations!!")
+#print("defining nes combinations!!")
 nesComb <- indexes(nrow(SPnes), nrow(Pnes))
 
-print("defining hosts answers!!")
+#print("defining hosts answers!!")
 hostsDF <- hostsAnswers(hostsComb, priceHostPerDay)
-print("defining links answers!!")
+#print("defining links answers!!")
 linksDF <- linksAnswers(linksComb, priceLinkPerDay)
-print("defining nes answers!!")
+#print("defining nes answers!!")
 nesDF <- nesAnswers(nesComb, priceNEPerDay)
 
 if ( nrow(hostsDF) == 0 | nrow(linksDF) == 0 | nrow(nesDF) == 0 ) { print("There is no valid answer!!") }
