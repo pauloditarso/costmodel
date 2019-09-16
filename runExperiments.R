df79d6d435c79144277964d869b74ee14859c3ab
Paulo Ditarso <paulo.maciel@ifpb.edu.br>
2019-09-16 17:42:36 +0300

    updating

--- runExperiments.R
+++ runExperiments.R
@@ -2,7 +2,7 @@ rm(list = ls())
 source('./sourceAll.R')
 SPConfig<-c(2,2,1)
 numberOfProviders <- 0
-numberOfTurns <- 2
+numberOfTurns <- 5
 priceHostPerDay <- 10000
 priceLinkPerDay <- 10000
 priceNEPerDay <- 10000
