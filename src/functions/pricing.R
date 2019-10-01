673cce87c244c2edf517583020917de2ebdd0a97
Paulo Ditarso <paulo.maciel@ifpb.edu.br>
2019-10-01 17:35:21 -0300

    updating pricing functions

--- src/functions/pricing.R
+++ src/functions/pricing.R
@@ -4,7 +4,8 @@ pricing <- function(arg1, arg2, arg3, resource) {
   auxVector <- c(arg1, arg2, arg3)
   
   if ( resource == "hosts" ) price <- sum(auxVector/referenceHost[1:3])/3 * referenceHost[4]
-  if ( resource == "links" ) price <- ( auxVector[1] + ( sum(referenceLink[2:3]/auxVector[2:3])/2 ) ) * referenceLink[4]
+#  if ( resource == "links" ) price <- ( auxVector[1] + ( sum(referenceLink[2:3]/auxVector[2:3])/2 ) ) * referenceLink[4]
+  if ( resource == "links" ) price <- sum(auxVector/referenceLink[1:3])/3 * referenceLink[4]
   if ( resource == "nes" ) price <- sum(auxVector/referenceNE[1:3])/3 * referenceNE[4]
   
   return(price)
