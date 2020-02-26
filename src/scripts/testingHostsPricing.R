7ff7905408f54a88fe84f935dede9c607c0a5373
Paulo Ditarso <paulo.maciel@ifpb.edu.br>
2020-02-26 17:20:18 -0300

    updating pricing tests

--- src/scripts/testingHostsPricing.R
+++ src/scripts/testingHostsPricing.R
@@ -1,13 +1,16 @@
-for ( i in c(1, 2, 4, 8, 16, 32) ) {
-  
-  for ( j in c(4, 8, 16, 32, 64, 128) ) {
-    
-    for ( k in c(128, 256, 512, 1024, 2048, 4096) ) {
-      
-      print(pricing(i, j, k, "hosts"))
-      
+source('~/Workspace/R-workspace/marketplace/costmodel/src/functions/pricing.R')
+
+hostsPricing <- vector()
+referenceHost <- c(1, 4, 128, 0.1)
+referenceLink <- c(1, 1, 1, 2)
+referenceNE <- c(1, 6, 1, 2)
+
+for ( cpu in c(1, 2, 4, 8, 16, 32) ) {
+  for ( mem in c(4, 8, 16, 32, 64, 128) ) {
+    for ( str in c(128, 256, 512, 1024, 2048, 4096) ) {
+      hostsPricing <- c(hostsPricing, pricing(cpu, mem, str, "hosts"))
     }
-    
   }
-  
 }
+
+rm( list = setdiff(ls(), ls(pattern = "Pricing")) )
--- src/scripts/testingLinksPricing.R
+++ src/scripts/testingLinksPricing.R
@@ -1,13 +1,16 @@
+source('~/Workspace/R-workspace/marketplace/costmodel/src/functions/pricing.R')
+
+linksPricing <- vector()
+referenceHost <- c(1, 4, 128, 0.1)
+referenceLink <- c(1, 1, 1, 2)
+referenceNE <- c(1, 6, 1, 2)
+
 for ( i in c(1, 2, 3, 4) ) {
-  
   for ( j in c(1, 2, 3, 4) ) {
-    
     for ( k in c(1, 2, 3, 4) ) {
-      
-      print(pricing(i, j, k, "links"))
-      
+      linksPricing <- c(linksPricing, pricing(i, j, k, "links"))
     }
-    
   }
-  
 }
+
+rm( list = setdiff(ls(), ls(pattern = "Pricing")) )
