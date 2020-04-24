9a45c931ff7187095e1d67c7f4d99f06640a23b7
Paulo Ditarso <paulo.maciel@ifpb.edu.br>
2020-04-24 12:29:30 -0300

    updating indexes

--- src/functions/indexes.R
+++ src/functions/indexes.R
@@ -14,24 +14,42 @@ indexes <- function(numberOfRequests, numberOfResources, quota) {
     requests <- gtools::permutations(numberOfRequests, numberOfRequests)
     resources <- gtools::combinations(numberOfResources, numberOfRequests)
     
-    for ( i in 1:nrow(requests) ) {
+    # for ( i in 1:nrow(requests) ) {
+    #   
+    #   for ( j in 1:nrow(resources) ) {
+    #     aux <- c(requests[i,], resources[j,])
+    #     allCombinations <- rbind(allCombinations, aux)
+    #     
+    #   }
+    # 
+    # }
+    
+    count <- 1
+    occurrences <- data.frame(i=character(0), j=character(0))
+    condition <- min(totalLength, quota)
+    
+    while (count <= condition) {
+      i <- sample(nrow(requests), 1)
+      j <- sample(nrow(resources), 1)
+      auxDF <- data.frame(i, j)
       
-      for ( j in 1:nrow(resources) ) {
-        aux <- c(requests[i,], resources[j,])
-        allCombinations <- rbind(allCombinations, aux)
-        
+      if ( duplicated(rbind(occurrences, auxDF))[nrow(occurrences)+1] ) {
+        next
       }
-  
+      aux <- c(requests[i,], resources[j,])
+      allCombinations <- rbind(allCombinations, aux)
+      occurrences <- rbind(occurrences, auxDF)
+      count <- count + 1
     }
     
     rm(i,j)
-    # allCombinations
-    if ( quota == -1 ) {
-      allCombinations
-    }
-    else {
-      allCombinations[c(sample(nrow(allCombinations),min(quota,totalLength),replace = FALSE)),]
-    }
+    allCombinations
+    # if ( quota == -1 ) {
+    #   allCombinations
+    # }
+    # else {
+    #   allCombinations[c(sample(nrow(allCombinations),min(quota,totalLength),replace = FALSE)),]
+    # }
 
   }
 
